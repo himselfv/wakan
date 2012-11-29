4b941bb72d0aab602a133fe2340a04e578172496
himselfv@gmail.com <himselfv@gmail.com>
2012-11-29 10:39:16 +0000

    Automatically take version info from resources. Upped version to "1.80 (dev)"

--- JWBMenu.pas
+++ JWBMenu.pas
@@ -560,14 +560,17 @@ var
   dictbeginset,dictmodeset:integer;
   kanji_othersearch:integer;
 
-const CurStructVer=2;
-      CurDictVer=7;
-      CurDicVer=4;
-      WakanVer='1.70a3';
+var
+  WakanVer: string = ''; //taken from resources on load
 
-      PopupButtonNum=4;
-      PopupButtonWidth=23;
-      PopupButtonSep=2;
+const
+  CurStructVer=2;
+  CurDictVer=7;
+  CurDicVer=4;
+
+  PopupButtonNum=4;
+  PopupButtonWidth=23;
+  PopupButtonSep=2;
 
 function _l(id:string):string;
 
@@ -585,6 +588,94 @@ uses JWBKanji, StdPrompt, JWBUnit, JWBRadical,
   JWBWordsExpChoose, JWBMedia, JWBDicSearch;
 
 {$R *.DFM}
+
+{ Utils }
+
+(*
+  Returns full image address for specified module
+  If hModule is zero, returns full executable image address
+*)
+function GetModuleFilenameStr(hModule: HMODULE = 0): string;
+const MAX_PATH_LEN = 8192; //Max length, in symbols, of supported image path size.
+var nSize, nRes: dword;
+begin
+ (*
+   MSDN:
+    If the length of the path is less than nSize characters, the function succeeds
+    and the path is returned as a null-terminated string.
+
+    If the length of the path exceeds nSize, the function succeeds and the string
+    is truncated to nSize characters including the terminating null character.
+
+    Windows XP/2000: The string is truncated to nSize characters and is not null terminated.
+ *)
+
+  nSize := 256;
+  SetLength(Result, nSize);
+
+  nRes := GetModuleFilename(hModule, @Result[1], nSize);
+  while (nRes <> 0) and (nRes >= nSize) and (nSize < MAX_PATH_LEN) do begin
+    nSize := nSize * 2;
+    SetLength(Result, nSize+1);
+    nRes := GetModuleFilename(hModule, @Result[1], nSize);
+  end;
+
+  if nRes = 0 then begin
+    Result := ''; //cannot retrieve path, return null
+    exit;
+  end;
+
+  if nRes >= nSize then begin
+    Result := ''; //path too long, exceeded MAX_PATH_LEN and still not enough, return null
+    exit;
+  end;
+
+  SetLength(Result, nRes); //else truncate the string, set terminating null
+end;
+
+function GetFileVersionInfoStr(Filename: string): string;
+var verSize: dword;
+  verHandle: dword;
+  verData: array of byte;
+  buf: pointer;
+  len: cardinal;
+begin
+  verSize := GetFileVersionInfoSize(PChar(Filename), verHandle);
+  if verSize=0 then begin
+    Result := '';
+    exit;
+  end;
+
+  buf := nil;
+  len := 0;
+  SetLength(verData, verSize);
+  if not GetFileVersionInfo(PChar(Filename), verHandle, verSize, @verData[0]) then begin
+    Result := '';
+    exit;
+  end;
+
+  if not VerQueryValue(@verData[0], '\', buf, len)
+  or (len=0) then begin
+    Result := '';
+    exit;
+  end;
+
+  with PVSFixedFileInfo(buf)^ do begin
+   //dwFileVersionMS: Major version (HIWORD) + Minor version (LOWORD)
+   //dwFileVersionLS: Release (HIWORD) + Build (LOWORD)
+    Result :=
+      IntToStr(HIWORD(dwFileVersionMS)) + '.' + IntToStr(LOWORD(dwFileVersionMS));
+    if dwFileFlags and VS_FF_PRERELEASE = VS_FF_PRERELEASE then
+      Result := Result + ' (dev)';
+    if dwFileFlags and VS_FF_DEBUG = VS_FF_DEBUG then
+      Result := Result + ' (debug)';
+    if dwFileFlags and VS_FF_SPECIALBUILD = VS_FF_SPECIALBUILD then
+      Result := Result + ' (special build)';
+  end;
+end;
+
+
+{ TfMenu }
 
 function TfMenu.GetCharDet(i,j:integer):string;
 var s:string;
@@ -5154,4 +5245,6 @@ initialization
   rdcnt:=0;
   inproc:=false;
   popcreated:=false;
+  WakanVer := GetFileVersionInfoStr(GetModuleFilenameStr(0));
+
 end.
