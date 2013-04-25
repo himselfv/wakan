2815201e6cd2c3c35d7739dcc5b2649499790d86
himselfv@gmail.com <himselfv@gmail.com>
2013-04-25 12:35:51 +0000

    Forgot this unit

---
+++ Controls/CheckAction.pas
@@ -0,0 +1,95 @@
+unit CheckAction;
+
+interface
+uses Classes, ActnList;
+
+{
+Sometimes we want to have centralized handling of some feature (ON/OFF),
+but we also want to do additional processing when the state is changed manually.
+
+I.e.:
+ - show/hide some forms and update some controls unconditionally when Checked is changed
+ - but only do some additional processing when Execute is called
+
+This TAction descendant gives us two handlers:
+  OnExecute to handle Action.Execute or button click/check
+  OnChecked to handle any Action.Checked change, no matter what triggered it
+}
+
+type
+ { Fortunately, there's already a way to receive some notifications through TActionLink }
+  TCheckAction = class;
+
+  TCheckActionLink = class(TActionLink)
+  protected
+    FClient: TCheckAction;
+    procedure AssignClient(AClient: TObject); override;
+    procedure SetChecked(Value: Boolean); override;
+  end;
+
+  TCheckAction = class(TAction)
+  protected
+    FOnChecking: TNotifyEvent;
+    FOnChecked: TNotifyEvent;
+    SelfLink: TCheckActionLink;
+    procedure Change; override;
+  public
+    constructor Create(AOwner: TComponent); override;
+  published
+    property OnChecking: TNotifyEvent read FOnChecking write FOnChecking;
+    property OnChecked: TNotifyEvent read FOnChecked write FOnChecked;
+  end;
+
+procedure Register;
+
+implementation
+
+procedure TCheckActionLink.AssignClient(AClient: TObject);
+begin
+  FClient := AClient as TCheckAction;
+end;
+
+procedure TCheckActionLink.SetChecked(Value: Boolean);
+begin
+ { TActionLink.SetChecked is called while FChecked is not yet changed,
+  so we cannot fire OnChecked() -- everybody will try TAction.Checked and fail }
+  if Assigned(FClient.FOnChecking) then
+    FClient.FOnChecking(FClient);
+ { We'll have to find some other point at which to call OnChecked() }
+end;
+
+constructor TCheckAction.Create(AOwner: TComponent);
+begin
+  inherited;
+  SelfLink := TCheckActionLink.Create(Self);
+  Self.RegisterChanges(SelfLink);
+end;
+
+type
+  TCustomActionHack = class helper for TCustomAction
+  protected
+    function GetChecking: boolean;
+  public
+    property Checking: boolean read GetChecking;
+  end;
+
+function TCustomActionHack.GetChecking: boolean;
+begin
+  Result := Self.FChecking;
+end;
+
+procedure TCheckAction.Change;
+begin
+  inherited;
+ { Change() is called on many occasions but we only need the call made from SetChecked().
+  Let's hope this suffices as a test: }
+  if Checking then
+    if Assigned(FOnChecked) then FOnChecked(Self);
+end;
+
+procedure Register;
+begin
+  RegisterActions('', [TCheckAction], nil);
+end;
+
+end.
