07b1389f1b5840d8c422314790d922ebf14bb48a
himselfv@gmail.com <himselfv@gmail.com>
2013-12-17 14:54:58 +0000

    Forgot this form!

---
+++ Forms/JWBWordLookupBase.dfm
@@ -0,0 +1,164 @@
+object fWordLookupBase: TfWordLookupBase
+  Left = 0
+  Top = 0
+  BorderStyle = bsNone
+  ClientHeight = 178
+  ClientWidth = 468
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Bevel: TPanel
+    Left = 0
+    Top = 0
+    Width = 468
+    Height = 178
+    Align = alClient
+    BevelInner = bvRaised
+    BevelOuter = bvLowered
+    FullRepaint = False
+    TabOrder = 0
+    DesignSize = (
+      468
+      178)
+    object btnGoToVocab: TSpeedButton
+      Left = 189
+      Top = 149
+      Width = 94
+      Height = 22
+      Hint = '#00931^eGo to this word in vocabulary'
+      Anchors = [akRight, akBottom]
+      Caption = '#00215^eVocabulary'
+      Glyph.Data = {
+        76010000424D7601000000000000760000002800000020000000100000000100
+        04000000000000010000120B0000120B00001000000000000000000000000000
+        800000800000008080008000000080008000808000007F7F7F00BFBFBF000000
+        FF0000FF000000FFFF00FF000000FF00FF00FFFF0000FFFFFF00333333333333
+        3333333333333333333333333333333333333FFF333333333333000333333333
+        3333777FFF3FFFFF33330B000300000333337F777F777773F333000E00BFBFB0
+        3333777F773333F7F333000E0BFBF0003333777F7F3337773F33000E0FBFBFBF
+        0333777F7F3333FF7FFF000E0BFBF0000003777F7F3337777773000E0FBFBFBF
+        BFB0777F7F33FFFFFFF7000E0BF000000003777F7FF777777773000000BFB033
+        33337777773FF733333333333300033333333333337773333333333333333333
+        3333333333333333333333333333333333333333333333333333333333333333
+        3333333333333333333333333333333333333333333333333333}
+      NumGlyphs = 2
+      ParentShowHint = False
+      ShowHint = True
+      OnClick = btnGoToVocabClick
+      ExplicitLeft = 425
+      ExplicitTop = 187
+    end
+    object btnAddToVocab: TSpeedButton
+      Left = 285
+      Top = 149
+      Width = 94
+      Height = 22
+      Hint = '#00667^eInsert word into vocabulary'
+      Anchors = [akRight, akBottom]
+      Caption = '#00215^eVocabulary'
+      Glyph.Data = {
+        76010000424D7601000000000000760000002800000020000000100000000100
+        04000000000000010000130B0000130B00001000000000000000000000000000
+        800000800000008080008000000080008000808000007F7F7F00BFBFBF000000
+        FF0000FF000000FFFF00FF000000FF00FF00FFFF0000FFFFFF00333333333333
+        33333333FF33333333FF333993333333300033377F3333333777333993333333
+        300033F77FFF3333377739999993333333333777777F3333333F399999933333
+        33003777777333333377333993333333330033377F3333333377333993333333
+        3333333773333333333F333333333333330033333333F33333773333333C3333
+        330033333337FF3333773333333CC333333333FFFFF77FFF3FF33CCCCCCCCCC3
+        993337777777777F77F33CCCCCCCCCC3993337777777777377333333333CC333
+        333333333337733333FF3333333C333330003333333733333777333333333333
+        3000333333333333377733333333333333333333333333333333}
+      NumGlyphs = 2
+      ParentShowHint = False
+      ShowHint = True
+      OnClick = btnAddToVocabClick
+      ExplicitLeft = 521
+      ExplicitTop = 187
+    end
+    object btnCopyToClipboard: TSpeedButton
+      Left = 381
+      Top = 149
+      Width = 80
+      Height = 22
+      Hint = '#00659^eInsert word into clipboard'
+      Anchors = [akRight, akBottom]
+      Caption = '#00660^eClipboard'
+      Glyph.Data = {
+        76010000424D7601000000000000760000002800000020000000100000000100
+        04000000000000010000130B0000130B00001000000000000000000000000000
+        800000800000008080008000000080008000808000007F7F7F00BFBFBF000000
+        FF0000FF000000FFFF00FF000000FF00FF00FFFF0000FFFFFF00333333333333
+        33333333FF33333333FF333993333333300033377F3333333777333993333333
+        300033F77FFF3333377739999993333333333777777F3333333F399999933333
+        33003777777333333377333993333333330033377F3333333377333993333333
+        3333333773333333333F333333333333330033333333F33333773333333C3333
+        330033333337FF3333773333333CC333333333FFFFF77FFF3FF33CCCCCCCCCC3
+        993337777777777F77F33CCCCCCCCCC3993337777777777377333333333CC333
+        333333333337733333FF3333333C333330003333333733333777333333333333
+        3000333333333333377733333333333333333333333333333333}
+      NumGlyphs = 2
+      ParentShowHint = False
+      ShowHint = True
+      OnClick = btnCopyToClipboardClick
+      ExplicitLeft = 617
+      ExplicitTop = 187
+    end
+    object BlankPanel: TBlankPanel
+      Left = 8
+      Top = 9
+      Width = 451
+      Height = 140
+      Anchors = [akLeft, akTop, akRight, akBottom]
+      TextLeft = 119
+      TextTop = 11
+      Text = '#00155^eNo words were found.'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -16
+      Font.Name = 'Arial'
+      Font.Style = [fsBold]
+    end
+    object StringGrid: TWakanWordGrid
+      Left = 9
+      Top = 10
+      Width = 449
+      Height = 138
+      Anchors = [akLeft, akTop, akRight, akBottom]
+      BorderStyle = bsNone
+      ColCount = 3
+      DefaultRowHeight = 16
+      DefaultDrawing = False
+      FixedCols = 0
+      Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goDrawFocusSelected, goColSizing, goRowSelect, goThumbTracking]
+      PopupMenu = pmHeader
+      TabOrder = 0
+      OnDblClick = StringGridDblClick
+      OnDrawCell = StringGridDrawCell
+      OnKeyPress = StringGridKeyPress
+      OnMouseDown = StringGridMouseDown
+      OnMouseMove = StringGridMouseMove
+      OnMouseUp = StringGridMouseUp
+      OnSelectCell = StringGridSelectCell
+      ColWidths = (
+        110
+        138
+        177)
+    end
+  end
+  object pmHeader: TPopupMenu
+    OnPopup = pmHeaderPopup
+    Left = 24
+    Top = 16
+    object miResetColumns: TMenuItem
+      Caption = '#01002^eReset columns'
+      OnClick = miResetColumnsClick
+    end
+  end
+end
---
+++ Forms/JWBWordLookupBase.pas
@@ -0,0 +1,358 @@
+unit JWBWordLookupBase;
+{ Basis for forms which list word lookup results: fWordLookup, fKanjiCompounds }
+
+interface
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Grids, WakanWordGrid, StdCtrls, WakanPaintbox, Buttons, ExtCtrls,
+  Menus, JWBDicSearch;
+
+type
+  TfWordLookupBase = class(TForm)
+    Bevel: TPanel;
+    BlankPanel: TBlankPanel;
+    StringGrid: TWakanWordGrid;
+    btnGoToVocab: TSpeedButton;
+    btnAddToVocab: TSpeedButton;
+    btnCopyToClipboard: TSpeedButton;
+    pmHeader: TPopupMenu;
+    miResetColumns: TMenuItem;
+    procedure pmHeaderPopup(Sender: TObject);
+    procedure miResetColumnsClick(Sender: TObject);
+    procedure StringGridDrawCell(Sender: TObject; ACol, ARow: Integer;
+      Rect: TRect; State: TGridDrawState);
+    procedure StringGridMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure StringGridMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure StringGridMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure btnGoToVocabClick(Sender: TObject);
+    procedure btnAddToVocabClick(Sender: TObject);
+    procedure btnCopyToClipboardClick(Sender: TObject);
+    procedure StringGridKeyPress(Sender: TObject; var Key: Char);
+    procedure StringGridDblClick(Sender: TObject);
+    procedure StringGridSelectCell(Sender: TObject; ACol, ARow: Integer;
+      var CanSelect: Boolean);
+
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
+  protected
+   { You don't have to retrieve the results from the dictionary, you may fill
+    it manually if you prefer }
+    FResults: TSearchResults;
+    procedure ResultsChanged; virtual;
+    procedure WordSelectionChanged; virtual;
+  public
+   { Currently selected word, cached for simplicity. Some rely on it outside
+    of the form, such as JWBHint on JWBWordLookup.curword }
+    curword: integer; //line number in string grid
+    curkanji: string;
+    curphonetic: string;
+    curmeaning: string;
+    procedure SetDefaultColumnWidths; virtual;
+    procedure Clear; virtual;
+    procedure CopyToClipboard(const AFullArticle, AReplace: boolean);
+    property Results: TSearchResults read FResults;
+
+
+  end;
+
+var
+  fWordLookupBase: TfWordLookupBase;
+
+
+implementation
+uses UITypes, JWBStrings, JWBUnit, JWBMenu, JWBCategories, JWBVocab, JWBVocabAdd;
+
+{$R *.dfm}
+
+constructor TfWordLookupBase.Create(AOwner: TComponent);
+begin
+  inherited;
+  FResults:=TSearchResults.Create; //it is sometimes used even before FormCreate, somehow
+end;
+
+destructor TfWordLookupBase.Destroy;
+begin
+  FreeAndNil(FResults);
+  inherited;
+end;
+
+procedure TfWordLookupBase.SetDefaultColumnWidths;
+begin
+  StringGrid.ColWidths[0]:=110;
+  StringGrid.ColWidths[1]:=138;
+  StringGrid.ColWidths[2]:=353;
+  StringGrid.AutoSizeColumns;
+end;
+
+procedure TfWordLookupBase.Clear;
+var sl: TStringList;
+begin
+ //Clear StringGrid the way wakan handles it.
+ //We can simply hide it (thats what FillWordGrid will do), but let's play it nice
+  sl := TStringList.Create;
+  try
+    FillWordGrid(StringGrid,sl,false,false);
+  finally
+    FreeAndNil(sl);
+  end;
+
+  FResults.Clear;
+
+  curword := 0;
+  WordSelectionChanged;
+end;
+
+procedure TfWordLookupBase.pmHeaderPopup(Sender: TObject);
+var p: TPoint;
+  ACol, ARow: integer;
+begin
+  p := StringGrid.ScreenToClient(Mouse.CursorPos);
+  StringGrid.MouseToCell(p.X, p.Y, ACol, ARow);
+  miResetColumns.Visible := (ARow=0); //click on header
+end;
+
+procedure TfWordLookupBase.miResetColumnsClick(Sender: TObject);
+begin
+  SetDefaultColumnWidths;
+end;
+
+procedure TfWordLookupBase.StringGridDrawCell(Sender: TObject; ACol,
+  ARow: Integer; Rect: TRect; State: TGridDrawState);
+begin
+  DrawWordCell(TStringGrid(Sender),ACol,ARow,Rect,State);
+end;
+
+procedure TfWordLookupBase.StringGridMouseMove(Sender: TObject;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  fMenu.IntTipMouseMove(TStringGrid(Sender),x,y,ssLeft in Shift);
+end;
+
+procedure TfWordLookupBase.StringGridMouseDown(Sender: TObject;
+  Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  if mbRight=Button then fMenu.PopupImmediate(false);
+end;
+
+procedure TfWordLookupBase.StringGridMouseUp(Sender: TObject;
+  Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  if mbLeft=Button then fMenu.IntTipMouseUp;
+end;
+
+procedure TfWordLookupBase.StringGridKeyPress(Sender: TObject; var Key: Char);
+begin
+ //Copy the article to clipboard on Ctrl-C
+  if (Key=^C) and (StringGrid.Visible) then begin
+    CopyToClipboard(
+      {full=}GetKeyState(VK_SHIFT) and $F0 = 0, //word-only when Shift pressed
+      {replace=}true //always replace
+      );
+    Key := #00;
+  end;
+end;
+
+procedure TfWordLookupBase.StringGridDblClick(Sender: TObject);
+begin
+  if btnAddToVocab.Enabled then btnAddToVocabClick(sender);
+end;
+
+procedure TfWordLookupBase.StringGridSelectCell(Sender: TObject; ACol,
+  ARow: Integer; var CanSelect: Boolean);
+begin
+ { Careful not to trigger an endless loop: WordSelectionChanged changes the grid
+  which triggers SelectCell which triggers WordSelectionChanged.
+  SelectCell also gets called endlessly while you hold mouse down due to a bug
+  in TStringGrid if RowSelect is true }
+  if curword=ARow then exit;
+  curword:=ARow;
+  WordSelectionChanged;
+end;
+
+{ Called when an entry has been selected in the result list }
+procedure TfWordLookupBase.WordSelectionChanged;
+begin
+ //Simple implementation. Descendants are free to make this more complicated:
+  if (curword>=1) and (curword<=FResults.Count) then begin
+    curphonetic:=remexcl(copy(StringGrid.Cells[0,curword],2,length(StringGrid.Cells[0,curword])-1));
+    curkanji:=remexcl(copy(StringGrid.Cells[1,curword],2,length(StringGrid.Cells[1,curword])-1));
+    curmeaning:=remexcl(StringGrid.Cells[2,curword]);
+    btnCopyToClipboard.Enabled:=true;
+    btnAddToVocab.Enabled:=true;
+    btnGoToVocab.Enabled:= (FResults[curword-1].userIndex<>0);
+  end else begin
+    curphonetic:='';
+    curkanji:='';
+    curmeaning:='';
+    btnGoToVocab.Enabled:=false;
+    btnCopyToClipboard.Enabled:=false;
+    btnAddToVocab.Enabled:=false;
+  end;
+end;
+
+{ Called when a Results list has been changed and the interface needs to be
+ updated }
+procedure TfWordLookupBase.ResultsChanged;
+var tmp: TStringList;
+  i: integer;
+begin
+ //Repopulate StringGrid
+  tmp := TStringList.Create;
+  try
+    for i:=0 to FResults.Count - 1 do
+      tmp.Add(FResults[i].ArticlesToString);
+    FillWordGrid(StringGrid,tmp,false,false);
+  finally
+    FreeAndNil(tmp);
+  end;
+end;
+
+{ Reformats combined article text a bit: moves all flags into a single clause,
+ adds linebreaks etc }
+function GlorifyCopypaste(const s: string): string;
+var ps, pc: PChar;
+  flags: string;
+
+  procedure CommitText;
+  begin
+    if pc<=ps then exit;
+    Result := Result + spancopy(ps,pc);
+  end;
+
+  procedure CommitTrimSpace;
+  begin
+   //We're going to cut something from this point on,
+   //so maybe trim a space at the end of the previous block
+    Dec(pc);
+    if pc^=' ' then begin
+      CommitText;
+      Inc(pc);
+    end else begin
+      Inc(pc);
+      CommitText;
+    end;
+  end;
+
+  procedure CommitFlags;
+  begin
+    if flags<>'' then
+      Result := Result + ' <'+flags+'>';
+    flags := '';
+  end;
+
+  procedure IncTrimSpace(var pc: PChar);
+  begin
+   //We have cut something before this point in the string,
+   //so maybe trim a space at the start of the next block
+    Inc(pc);
+    if pc^=' ' then begin
+      Inc(pc);
+      if pc^<>' ' then
+        Dec(pc);
+    end;
+  end;
+
+begin
+  if s='' then begin
+    Result := '';
+    exit;
+  end;
+
+  flags := '';
+
+  pc := PChar(s);
+  ps := pc;
+  while pc^<>#00 do
+
+    if pc^='<' then begin
+      CommitTrimSpace;
+      Inc(pc);
+      ps := pc;
+      while (pc^<>#00) and (pc^<>'>') do
+        Inc(pc);
+      if pc>ps then
+       //First char of the flag is always its type; we don't care
+        Inc(ps);
+      if pc>ps then begin
+        if flags<>'' then
+          flags := flags + ', ';
+        flags := flags + spancopy(ps,pc);
+      end;
+      IncTrimSpace(pc);
+      ps := pc;
+    end else
+
+    if pc^='/' then begin
+      CommitTrimSpace;
+      CommitFlags;
+      if (Result<>'') and (Result[Length(Result)]<>#13) then
+        Result := Result + #13;
+      IncTrimSpace(pc);
+      ps := pc;
+    end else
+
+    if (pc^='(') and IsDigit(pc[1]) and (
+      (pc[2]=')') or ( IsDigit(pc[2]) and (pc[3]=')')  )
+    ) then begin
+      CommitTrimSpace;
+      CommitFlags;
+      if (Result<>'') and (Result[Length(Result)]<>#13) then
+        Result := Result + #13;
+      ps := pc; //include brackets too
+      Inc(pc);
+    end else
+
+      Inc(pc); //any other char
+
+  CommitText;
+  CommitFlags;
+end;
+
+//Copies currently selected article to the clipboard
+procedure TfWordLookupBase.CopyToClipboard(const AFullArticle, AReplace: boolean);
+var AText, tmp: string;
+begin
+  if AFullArticle then begin
+    tmp := curmeaning;
+    if pos(' >> ',tmp)>0 then delete(tmp,1,pos(' >> ',tmp)+3);
+    tmp:=UnfixVocabEntry(tmp); //replace markup symbols with user readable
+    tmp:=GlorifyCopypaste(tmp); //reformat a bit to make it more pleasant
+    AText:=curkanji+' ['+curphonetic+'] '+tmp;
+  end else
+    AText:=curkanji;
+  if AReplace then
+    clip := AText
+  else
+    clip := clip + AText;
+  fMenu.SetClipboard;
+end;
+
+procedure TfWordLookupBase.btnGoToVocabClick(Sender: TObject);
+begin
+  fMenu.aModeWordsExecute(sender);
+  if FResults[curword-1].userIndex<>0 then
+    fVocab.SearchWord(FResults[curword-1].userIndex);
+end;
+
+procedure TfWordLookupBase.btnAddToVocabClick(Sender: TObject);
+var tmp: string;
+begin
+  tmp := curmeaning;
+  if pos(' >> ',tmp)>0 then delete(tmp,1,pos(' >> ',tmp)+3);
+  tmp:=UnfixVocabEntry(tmp); //replace markup symbols with user readable
+  if not IsPositiveResult(fVocabAdd.ModalAddFixed(curkanji,curphonetic,fstr(tmp))) then
+    exit;
+end;
+
+procedure TfWordLookupBase.btnCopyToClipboardClick(Sender: TObject);
+begin
+ //Emulate older behavior
+  CopyToClipboard({full=}false,{replace=}false);
+end;
+
+end.
